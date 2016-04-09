e86af866942d225af504768ba0f761a514272240
himaaaatti <sux2mfgj@gmail.com>
2016-04-09 18:44:07 +0900

    can work egg gacha but this have many problem

--- egggacha/egggacha.kt
+++ egggacha/egggacha.kt
@@ -8,63 +8,116 @@ import org.bukkit.Material
 import org.bukkit.block.Chest
 import org.bukkit.inventory.Inventory
 import org.bukkit.block.Block
+import org.bukkit.material.Button
+import org.bukkit.inventory.ItemStack
+
+import java.util.Random
+
+//import org.bukkit.command.Command;
+//import org.bukkit.command.CommandSender;
 
 class EggGacha: JavaPlugin() {
 
+    //TODO redstone, wood_button, leverなどのしょりを追加
+
+    val rand = Random()
 
     override fun onEnable() {
         
         val logger = getLogger()
-        logger.info("test")
 
         getServer().getPluginManager().registerEvents(
                 object: Listener{
                     @EventHandler
-                    fun onChangeSpawn(e: BlockRedstoneEvent) {
+                    fun onReceiveRedStonePower(e: BlockRedstoneEvent) 
+                    {
+                        if(e.getOldCurrent() >= e.getNewCurrent()) {
+                            return
+                        }
                         val block = e.getBlock()
-                        if (block.getType() == Material.IRON_BLOCK )
+
+                        if(block.getType() != Material.STONE_BUTTON) {
+                            return
+                        }
+                        val button = block.getState().getData() as Button
+                        val attached = block.getRelative(button.getAttachedFace())
+
+                        if (attached.getType() != Material.PUMPKIN)
                         {
-                            when(searchChest(block)) {
-                                CHEST_PLACE.X -> logger.info("X")
-                                CHEST_PLACE.Y -> logger.info("Y")
-                                CHEST_PLACE.NOT_FOUND -> logger.info("Not found")
+                            return
+                        }
+                        
+                        val receive_chest_block = attached.getRelative(0, 1, 0)
+                        if(receive_chest_block.getType() != Material.CHEST)
+                        {
+                            return
+                        }
+
+                        val send_chest_block :Block? = searchChest(attached)
+                        if(send_chest_block != null) {
+                            
+                            // get egg from receive_chest
+                            logger.info("found both chest")
+                            //TODO 
+                            val r_chest = receive_chest_block.getState() as Chest
+                            val r_invent = r_chest.getBlockInventory()
+                            val first_slot: Int = r_invent.first(Material.EGG)
+
+                            logger.info(first_slot.toString())
+                            if(first_slot == -1) {
+                                return
                             }
 
+                            val item_stack = r_invent.getItem(first_slot)
+                            item_stack.setAmount(item_stack.getAmount() - 1)
+                            //TODO 最後の一つの時にsetAmount(0)とやっても亡くならない
+
+                            // set item to send_chest
+                            val s_chest = send_chest_block.getState() as Chest
+                            val s_invent = s_chest.getBlockInventory()
+                            val first_empty = s_invent.firstEmpty()
+                            if(first_empty == -1) {
+                                //item_stack.setAmount(item_stack.getAmount() + 1)
+                                return
+                            }
+
+                            s_invent.setItem(first_empty, getRandomItem())
+
                         }
+
                     }
                 }, this
         )
 
     }
 
-    enum class CHEST_PLACE {
-        NOT_FOUND,
-        X,
-        Y,
+/*    override fun onCommand(sender: CommandSender,
+                    cmd: Command,
+                    label: String,
+                    args: Array<String>) : Boolean
+    {
+        return false
+    }
+    */
+
+    fun getRandomItem() :ItemStack {
+        val mat_array = Material.values()
+
+        val ran_index = rand.nextInt(mat_array.size)
+
+        return ItemStack(mat_array[ran_index])
     }
 
-    fun searchChest(block: Block) : CHEST_PLACE {
-        var x_p1_block = block.getRelative(1, 0, 0) 
-        var x_m1_block = block.getRelative(-1, 0, 0) 
-
-        if(x_p1_block.getType() == Material.CHEST && 
-            x_m1_block.getType() == Material.CHEST) 
-        {
-            getLogger().info("x = 1 & x = -1")
-            return CHEST_PLACE.X
+    fun searchChest(block: Block) : Block? {
+        for(x in -1..1) {
+            for(z in -1..1) {
+                val chest = block.getRelative(x, 0, z) 
+                if(chest.getType() == Material.CHEST) {
+                    return chest
+                }
+            }
         }
-        x_p1_block = block.getRelative(0, 0, 1) 
-        x_m1_block = block.getRelative(0, 0, -1) 
-
-        if(x_p1_block.getType() == Material.CHEST && 
-            x_m1_block.getType() == Material.CHEST) 
-        {
-            getLogger().info("z = 1 & z = -1")
-            return CHEST_PLACE.Y
-        }
-
-        getLogger().info("not found")
-        return CHEST_PLACE.NOT_FOUND
+        return null
     }
  
 }
