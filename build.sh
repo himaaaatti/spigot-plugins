e7bf967947c50cfe214118011f49c2ba29edc63d
himaaaatti <sux2mfgj@gmail.com>
2016-04-10 15:06:06 +0900

    add argument check

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+    echo "For example"
+    echo "  ./build.sh cut"
+    exit 1
+fi
 
 cd ${1}
-kotlinc -cp ../spigot-api-1.9.2.jar ${1}.kt -include-runtime -d ${1}.jar
+kotlinc -cp ../spigot-api-1.9.2.jar ${1}.kt -include-runtime -d ${1}.jar && \
 jar uf ${1}.jar plugin.yml
