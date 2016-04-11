556099a974b4a9b89f288ba40770ddffef0fd955
himaaaatti <sux2mfgj@gmail.com>
2016-04-12 00:05:26 +0900

    for config.yml

--- build.sh
+++ build.sh
@@ -9,3 +9,9 @@ fi
 cd ${1}
 kotlinc -cp ../spigot-api-1.9.2.jar ${1}.kt -include-runtime -d ${1}.jar && \
 jar uf ${1}.jar plugin.yml
+
+if [ -e config.yml ]; 
+then
+    jar uf ${1}.jar config.yml
+fi
+
